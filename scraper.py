21038edff91fde99c579a6d7f3b9460e5674263d
Shailusha <shailushakamsani@gmail.com>
2026-07-11 18:40:48 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,1 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
 
+url = "https://books.toscrape.com/"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    books = soup.find_all("article", class_="product_pod")
+
+    with open("products.csv", "w", newline="", encoding="utf-8") as file:
+
+        writer = csv.writer(file)
+
+        writer.writerow(["Product Name", "Price", "Rating"])
+
+        for book in books:
+
+            name = book.h3.a["title"]
+            price = book.find("p", class_="price_color").text
+            rating = book.find("p")["class"][1]
+
+            writer.writerow([name, price, rating])
+
+    print("Products saved successfully in products.csv")
+
+else:
+    print("Failed to connect to website.")
